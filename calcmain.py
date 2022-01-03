22a526eb64b9dd22a4adf57aa5d1f166bd088642
Pliszka <karoliniakkjk@gmail.com>
2022-01-03 15:37:02 +0100

    ładne kolory tj zielony i szaroniebieski

--- calcmain.py
+++ calcmain.py
@@ -10,16 +10,16 @@ def start():
 
 
 def screen():
-    scr = [tkinter.Label(root, width=70,fg='#333333', bg='#F15156', text="test", anchor='w', borderwidth=15) for i in range(3)]
+    scr = [tkinter.Label(root, width=70,fg='#333333', bg='#9EBD6E', text="test", anchor='w', borderwidth=15) for i in range(3)]
     for i in range(len(scr)):
         scr[i].grid(row=i, column=0, ipady=5, ipadx=1)
     return scr
 
 def input():
-    area=tkinter.Entry(root, bg='#D4F4DD', fg='#333333', borderwidth=0, highlightcolor='#333333', highlightbackground='#333333')
+    area=tkinter.Entry(root, bg='#C9DAEA', fg='#333333', borderwidth=0, highlightcolor='#333333', highlightbackground='#333333')
     area.grid(row=len(scr), column=0, ipady=15, ipadx=200)
 
-    info = tkinter.Label(root, width=70, bg='#D4F4DD', fg='#333333', text="test", anchor='w', borderwidth=15)
+    info = tkinter.Label(root, width=70, bg='#C9DAEA', fg='#333333', text="test", anchor='w', borderwidth=15)
     info.grid(row=len(scr)+1, column=0, ipady=5, ipadx=1)
 
     return area
